cfd5cc7c53f4d9fe6fc8d7acb9aa3d1b35576705
reachhwasup <paoponareach08@gmail.com>
2023-05-27 21:13:40 +0700

    update metadata

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <Suspense fallback={<Loading/>}> 
-            <Slideshow/>
+            {/* <Slideshow/> */}
             <Products/>
           <Categories/>
           <User/>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -20,12 +20,14 @@ const Navbar = () => {
             </span>
           </a>
           <div className="flex md:order-2 ">
+            <Link href={"/addproduct"}>
             <button
               type="button"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Add product
             </button>
+            </Link>
 
             <Link href={"/adduser"}>
             <button
